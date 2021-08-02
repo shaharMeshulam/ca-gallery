3dd378d46e275a555db677f22d5ab6fe44b117c9
shaharmeshulam <shahar.mesh@gmail.com>
2021-08-02 18:53:17 +0300

    first commit

---
+++ js/portfolio.service.js
@@ -0,0 +1,98 @@
+'use strict'
+
+var gPortfolio = [
+    {
+        'id': '1',
+        'name': 'Dr-Sagiv',
+        'title': 'Dr-Sagiv.co.il',
+        'url': 'https://dr-sagiv.co.il/',
+        'labels': ['Drupal', 'Html', 'Css'],
+        'thumbUrl': 'img/portfolio/dr-sagiv-thumb.png',
+        'fullImgUrl': 'img/portfolio/dr-sagiv-full.png'
+    },
+    {
+        'id': '2',
+        'name': 'Trader',
+        'title': 'Trader',
+        'url': 'https://shaharmeshulam.co.il/old/pages/protfolio/sites/trader',
+        'labels': ['Html', 'Css', 'BootStrap'],
+        'thumbUrl': 'img/portfolio/trader-thumb.png',
+        'fullImgUrl': 'img/portfolio/trader-full.png'
+    },
+    {
+        'id': '3',
+        'name': 'Itnahaguyut',
+        'title': 'Itnahaguyut',
+        'url': null,
+        'labels': ['Html', 'Css', 'BootStrap'],
+        'thumbUrl': 'img/portfolio/itnahaguyut-thumb.png',
+        'fullImgUrl': 'img/portfolio/itnahaguyut-full.png'
+    },
+    {
+        'id': '4',
+        'name': 'Itnahaguyut',
+        'title': 'Itnahaguyut',
+        'url': null,
+        'labels': ['Html', 'Css', 'BootStrap'],
+        'thumbUrl': 'img/portfolio/itnahaguyut-thumb.png',
+        'fullImgUrl': 'img/portfolio/itnahaguyut-full.png'
+    },
+    {
+        'id': '5',
+        'name': 'Yaad-ins',
+        'title': 'Yaad-ins.co.il',
+        'url': 'http://yaad-ins.co.il/',
+        'labels': ['Drupal', 'Html', 'Css'],
+        'thumbUrl': 'img/portfolio/yaad-ins-thumb.png',
+        'fullImgUrl': 'img/portfolio/yaad-ins-full.png'
+    },
+    {
+        'id': '6',
+        'name': 'Weather',
+        'title': 'Weather',
+        'url': 'https://shaharmeshulam.github.io/shahar-meshulam-19-01-20',
+        'labels': ['Angular', 'Html', 'Css'],
+        'thumbUrl': 'img/portfolio/weather-thumb.png',
+        'fullImgUrl': 'img/portfolio/weather-full.png'
+    },
+    {
+        'id': '7',
+        'name': 'BitInfo',
+        'title': 'BitInfo',
+        'url': 'https://shaharmeshulam.co.il/old/pages/protfolio/sites/lostbit/public',
+        'labels': ['Html', 'Css', 'BootStrap'],
+        'thumbUrl': 'img/portfolio/bit-info-thumb.png',
+        'fullImgUrl': 'img/portfolio/bit-info-full.png'
+    },
+    {
+        'id': '8',
+        'name': 'Communit',
+        'title': 'Communit',
+        'url': 'https://shaharmeshulam.co.il/old/pages/protfolio/sites/communit/',
+        'labels': ['Html', 'Css', 'BootStrap'],
+        'thumbUrl': 'img/portfolio/communit-thumb.png',
+        'fullImgUrl': 'img/portfolio/communit-full.png'
+    },
+    {
+        'id': '9',
+        'name': 'MineSweeper',
+        'title': 'MineSweeper',
+        'url': 'https://shaharmeshulam.github.io/MineSweeper/',
+        'labels': ['JavaScript', 'Html', 'Css'],
+        'thumbUrl': 'img/portfolio/mine-sweeper-thumb.png',
+        'fullImgUrl': 'img/portfolio/mine-sweeper-full.png'
+    },
+    {
+        'id': '10',
+        'name': 'Balloons',
+        'title': 'Balloons',
+        'url': 'https://shaharmeshulam.github.io/Balloons/',
+        'labels': ['JavaScript', 'Html', 'Css'],
+        'thumbUrl': 'img/portfolio/balloons-thumb.png',
+        'fullImgUrl': 'img/portfolio/balloons-full.png'
+    },
+];
+
+function getPortfolio() {
+    return gPortfolio;
+}
